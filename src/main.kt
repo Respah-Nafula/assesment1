0730a779be6f4fedcaddf25be64f96e61d0d7741
Respah-Nafula <nafularespah@gmail.com>
2022-03-10 09:13:18 +0300

    add file

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main(){
+    division(678,25)
+    parameters()
+    length()
+    text()
+}
+//function that takes in two numbers and returns out their remainder
+fun division(a:Int,b:Int){
+    var modulus=a%b
+    println(modulus)
+
+}
+fun parameters(){
+    var name='P'
+    var age='Z'
+    var Q=("Hi my name is $name and I am $age years old")
+    println(Q)
+}
+//function that takes in a string and outputs its length
+fun length(){
+    var Z="sentence"
+    var T="kotlin is a very friendly programming language"
+    println(T)
+}
+
+//function that takes in a string "codeHive" and prints its characters
+fun text(){
+    var text="codeHive"
+    var W=text[5].toString()+text[6]+text[7]+text[8]
+    println(W)
+}
